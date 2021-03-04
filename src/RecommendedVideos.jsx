30214412b2c3033d58e4e3d7e6a95d79453fda11
Aaron Yu <ayybuddyy@gmail.com>
2021-03-04 11:50:31 -0500

    Create VideoCard component and pass VideoCard component to RecommendedVideos component

--- src/RecommendedVideos.jsx
+++ src/RecommendedVideos.jsx
@@ -1,10 +1,61 @@
 import React from 'react';
 import './RecommendedVideos.css';
+import VideoCard from './VideoCard';
 
 const RecommendedVideos = () => {
   return (
     <div className='recommendedVideos'>
-      <h1>This is where videos go</h1>
+      <h2>Recommended</h2>
+      <div className='recommendedVideos__videos'>
+        <VideoCard
+          title='Getting into Shape and Dieting'
+          views='640K'
+          timestamp='6 months ago'
+          channelImage='https://i.insider.com/5af59dd07708e905f8336883?width=600&format=jpeg&auto=webp'
+          channel='Jocko Podcast'
+          image='https://i.ytimg.com/vi/a8_gxXEwd4Q/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDkbRFyruoW8Wl5a9tjYylEcV_J3g'
+        />
+        <VideoCard
+          title='Getting into Shape and Dieting'
+          views='640K'
+          timestamp='6 months ago'
+          channelImage='https://i.insider.com/5af59dd07708e905f8336883?width=600&format=jpeg&auto=webp'
+          channel='Jocko Podcast'
+          image='https://i.ytimg.com/vi/a8_gxXEwd4Q/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDkbRFyruoW8Wl5a9tjYylEcV_J3g'
+        />
+        <VideoCard
+          title='Getting into Shape and Dieting'
+          views='640K'
+          timestamp='6 months ago'
+          channelImage='https://i.insider.com/5af59dd07708e905f8336883?width=600&format=jpeg&auto=webp'
+          channel='Jocko Podcast'
+          image='https://i.ytimg.com/vi/a8_gxXEwd4Q/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDkbRFyruoW8Wl5a9tjYylEcV_J3g'
+        />
+        <VideoCard
+          title='Getting into Shape and Dieting'
+          views='640K'
+          timestamp='6 months ago'
+          channelImage='https://i.insider.com/5af59dd07708e905f8336883?width=600&format=jpeg&auto=webp'
+          channel='Jocko Podcast'
+          image='https://i.ytimg.com/vi/a8_gxXEwd4Q/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDkbRFyruoW8Wl5a9tjYylEcV_J3g'
+        />
+        <VideoCard
+          title='Getting into Shape and Dieting'
+          views='640K'
+          timestamp='6 months ago'
+          channelImage='https://i.insider.com/5af59dd07708e905f8336883?width=600&format=jpeg&auto=webp'
+          channel='Jocko Podcast'
+          image='https://i.ytimg.com/vi/a8_gxXEwd4Q/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDkbRFyruoW8Wl5a9tjYylEcV_J3g'
+        />
+        <VideoCard
+          title='Getting into Shape and Dieting'
+          views='640K'
+          timestamp='6 months ago'
+          channelImage='https://i.insider.com/5af59dd07708e905f8336883?width=600&format=jpeg&auto=webp'
+          channel='Jocko Podcast'
+          image='https://i.ytimg.com/vi/a8_gxXEwd4Q/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDkbRFyruoW8Wl5a9tjYylEcV_J3g'
+        />
+      </div>
     </div>
   );
 };
