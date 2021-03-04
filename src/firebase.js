80abc36c9607de80fbc3d85cf7527179ccfd83a4
Aaron Yu <ayybuddyy@gmail.com>
2021-03-04 09:54:40 -0500

    Clean up files, add react router dom, material ui and firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDnmO5BPE5VlIIk9Hwikj8IlOwE_WFhMS0',
+  authDomain: 'clone-c-b5c46.firebaseapp.com',
+  projectId: 'clone-c-b5c46',
+  storageBucket: 'clone-c-b5c46.appspot.com',
+  messagingSenderId: '739232572878',
+  appId: '1:739232572878:web:eddfbf48156eedb4d7f79f',
+  measurementId: 'G-EN9K9W6LBT',
+};
